a336455b49ca8d0530b1c5cad1cd1ea26d72afd5
Cengizhan <cengizhancivelek@gmail.com>
2023-10-31 18:01:08 +0300

    [Feat] Checkbox Model
    Created checkbox model

---
+++ Presentation/ReDoMusic.MVC/Models/CheckboxViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ReDoMusic.Core.Domain.Entities;
+using ReDoMusic.Core.Domain.Enums;
+
+namespace ReDoMusic.MVC.Models
+{
+    public class CheckboxViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSelected { get; set; }
+    }
+
+
+    public class InstrumentColorViewModel
+    {
+        public List<CheckboxViewModel> Instruments { get; set; }
+        public List<CheckboxViewModel> Colors { get; set; }
+        public List<Instrument> FilteredInstruments { get; set; }
+        public List<Color> FilteredColors { get; set; } 
+    }
+}
